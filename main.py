eca09949bbc90363b8fe6859bf2112a4800b7a61
Alvaro Ulloa <alvarouc@gmail.com>
2017-03-30 12:06:48 -0400

    fixed quantization

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def cat3(vector):
     """
     return pd.qcut(vector, [0,.33,.66,1]).codes
 
-def prep_data(n_samples=500, n_real=5, n_categorical=3, n_noisy=2, n_cluters=3):
+def prep_data(n_samples=500, n_real=5, n_categorical=3, n_noisy=2, n_clusters=3):
     """
     Prepares dataset for TSNE experiment
 
@@ -35,29 +35,34 @@ def prep_data(n_samples=500, n_real=5, n_categorical=3, n_noisy=2, n_cluters=3):
     - cat_bool = boolean index of categorical variables in X 
     """
 
-    n_features = n_features_cont + n_features_cat + n_features_noise
+    n_features = n_real + n_categorical
 
     # Make dataset
     X, y = make_classification(n_samples=n_samples,
                                n_features=n_features,
-                               n_informative=n_features_cont + n_features_cat,
-                               n_redundant=n_features_noise,
+                               n_informative=n_real + n_categorical,
                                n_classes=n_clusters,
+                               n_redundant=0,
+                               n_repeated=0,
                                class_sep=10,
                                n_clusters_per_class = 1,
                                shuffle=False,
                                random_state=1988)
+
+    X = np.concatenate([ X, np.random.random((n_samples, n_noisy))], axis=1)
+    
     logger.info('Generated dataset with shape {}'.format(X.shape))
     logger.info('Prediction score : {}'.format(
         np.mean(cross_val_score(RFC(), X, y))))
 
-    logger.info('Quantizing {} variables'.format(n_features_cat))
-    X[:,:n_features_cat] = np.apply_along_axis(
-        cat3, axis=0, arr=X[:,:n_features_cat])
-    logger.info('Prediction score after quantization : {}'.format(
-        np.mean(cross_val_score(RFC(), X, y))))
+    if n_categorical>0:
+        logger.info('Quantizing {} variables'.format(n_categorical))
+        X[:,:n_categorical] = np.apply_along_axis(
+            cat3, axis=0, arr=X[:,:n_categorical])
+        logger.info('Prediction score after quantization : {}'.format(
+            np.mean(cross_val_score(RFC(), X, y))))
 
-    cat_bool = np.arange(n_features)<n_features_cat
+    cat_bool = np.arange(n_features)<n_categorical
     return (X, y, cat_bool)
 
 
@@ -81,6 +86,9 @@ def main():
     #2. Most variables are predictive (n=8); some noise (n=2)
     #3. Equal number of predictive variables (n=5), noisy variables (n=5)
     #4. Most variables are noisy (n=8); small number of predictive variables (n=2)
+    n_samples = 500
+    n_clusters = 3
+    
     for n_noisy in [0,2,5,8]:
         n_predictive = 10 - n_noisy
 
@@ -88,11 +96,15 @@ def main():
         #-Cluster label predicted by categorical variables only
         #-Cluster label predicted by both categorical and quantitative variables
         for p in [0, .5, 1]:
-           n_categorical = round(n_feat * p)
+           n_categorical = round(n_predictive * p)
            n_real = n_predictive-n_categorical
-           logger.info('N_REAL: {}, N_CATEGORICAL: {}, N_NOISY: {}'.format(n_real, n_categorical, n_noisy))
-           X, y, cat_bool = prep_data(n_samples=5000,
-                                      n_cluters=3,
-                                      **(n_real, n_categorical, n_predictive))
+           logger.info('N_REAL: {}, N_CATEGORICAL: {}, N_NOISY: {}'\
+                       .format(n_real, n_categorical, n_noisy))
+           X, y, cat_bool = prep_data(*(n_samples, n_real,
+                                        n_categorical, n_noisy,
+                                        n_clusters))
 
+           
+if __name__== "__main__":
 
+    main()
