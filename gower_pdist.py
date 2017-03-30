37dfb16721f7d769ec99bb9f536e602a35a631cf
Alvaro Ulloa <alvarouc@gmail.com>
2017-03-30 10:49:04 -0400

    prep for sim

--- gower_pdist.py
+++ gower_pdist.py
@@ -9,9 +9,7 @@ def gower(xi,xj, cat_bool):
     con = np.sum(np.abs(xi[~cat_bool]-xj[~cat_bool]))
     return (cat+con)/len(xi)
 
-def compute_sim(X, cat_idx):
-    cat_bool = np.array([x in cat_idx for x in np.arange(0,X.shape[1])])
-
+def compute_sim(X, cat_bool):
     Xsd = (X - X.min(axis=0))/X.ptp(axis=0)
     d = pairwise_distances(Xsd, None,
                            partial(gower,
--- main.py
+++ main.py
@@ -18,43 +18,59 @@ def cat3(vector):
     """
     return pd.qcut(vector, [0,.33,.66,1]).codes
 
-# Simulation parameters
-N_SAMPLES = 500
-N_FEATURES_CONT = 5
-N_FEATURES_CAT = 1
-N_FEATURES_NOISE = 2
-N_FEATURES = N_FEATURES_CONT + N_FEATURES_CAT + N_FEATURES_NOISE
-N_CLUSTERS = 2
+def prep_data(n_samples=500, n_real=5, n_categorical=3, n_noisy=2, n_cluters=3):
+    """
+    Prepares dataset for TSNE experiment
+
+    Input parameters: 
+    - n_samples= number of total samples
+    - n_real = number of real variables
+    - n_categorical = number of categorical variables
+    - n_noisy = number of noisy variables
+    - n_clusters = number of clusters to create
+
+    Output: 
+    - X = dataset
+    - y = cluster labels
+    - cat_bool = boolean index of categorical variables in X 
+    """
+
+    n_features = n_features_cont + n_features_cat + n_features_noise
+
+    # Make dataset
+    X, y = make_classification(n_samples=n_samples,
+                               n_features=n_features,
+                               n_informative=n_features_cont + n_features_cat,
+                               n_redundant=n_features_noise,
+                               n_classes=n_clusters,
+                               class_sep=10,
+                               n_clusters_per_class = 1,
+                               shuffle=False,
+                               random_state=1988)
+    logger.info('Generated dataset with shape {}'.format(X.shape))
+    logger.info('Prediction score : {}'.format(
+        np.mean(cross_val_score(RFC(), X, y))))
+
+    logger.info('Quantizing {} variables'.format(n_features_cat))
+    X[:,:n_features_cat] = np.apply_along_axis(
+        cat3, axis=0, arr=X[:,:n_features_cat])
+    logger.info('Prediction score after quantization : {}'.format(
+        np.mean(cross_val_score(RFC(), X, y))))
+
+    cat_bool = np.arange(n_features)<n_features_cat
+    return (X, y, cat_bool)
 
 
-# Make dataset
-X, y = make_classification(n_samples=N_SAMPLES,
-                           n_features=N_FEATURES,
-                           n_informative=N_FEATURES_CONT + N_FEATURES_CAT,
-                           n_redundant=N_FEATURES_NOISE,
-                           n_classes=N_CLUSTERS,
-                           class_sep=10,
-                           n_clusters_per_class = 1,
-                           shuffle=False,
-                           random_state=1988)
-logger.info('X.shape  = {}'.format(X.shape))
+def compute_tsne(X, cat_bool):
 
-logger.info('Prediction score : {}'.format(
-    np.mean(cross_val_score(RFC(), X, y))))
-# quantize features
-X[:,:N_FEATURES_CAT] = np.apply_along_axis(
-    cat3, axis=0, arr=X[:,:N_FEATURES_CAT])
-logger.info('Prediction score after quantiation : {}'.format(
-    np.mean(cross_val_score(RFC(), X, y))))
+    dist = compute_sim(X, cat_bool)
 
-dist = compute_sim(X, cat_idx=np.arange(0,N_FEATURES_CAT))
-#from sklearn.metrics.pairwise import pairwise_distances
-#dist = pairwise_distances(X, None, 'euclidean')
-ts = TSNE(perplexity=10, metric='precomputed', early_exaggeration=500)
-X2 = ts.fit_transform(dist)
-logger.info('Prediction score tsne : {}'.format(
-    np.mean(cross_val_score(RFC(), X2, y))))
+    ts = TSNE(perplexity=30, metric='precomputed')
+    X2 = ts.fit_transform(dist)
 
-logger.info('KL divergence: {}'.format(ts.kl_divergence_))
-plt.scatter(X2[:,0], X2[:,1], c=y)
-plt.savefig('tsne_result.png')
+    logger.info('Prediction score tsne : {}'.format(
+        np.mean(cross_val_score(RFC(), X2, y))))
+
+    logger.info('KL divergence: {}'.format(ts.kl_divergence_))
+    plt.scatter(X2[:,0], X2[:,1], c=y, alpha=0.8, marker='.')
+    plt.savefig('tsne_result.png')
